917826d56c6ceef3480c20c0bfd6dd2d9e222644
ameen2vroot <mohammed.ameen@blackbuck.com>
2026-02-22 16:52:05 +0530

    initial commit

---
+++ src/test/java/com/example/blogmod/BlogmodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.blogmod;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogmodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
