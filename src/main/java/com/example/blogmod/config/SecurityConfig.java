30f5c6ea2256d09f7a1fbd6912d78bc5b787b177
ame2en <personal@email.com>
2026-02-23 22:37:19 +0530

    checking ame2en account

--- src/main/java/com/example/blogmod/config/SecurityConfig.java
+++ src/main/java/com/example/blogmod/config/SecurityConfig.java
@@ -10,6 +10,8 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 public class SecurityConfig {
 
+
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
